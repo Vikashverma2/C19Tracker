1103154c57bd29accd652ae163b9d2c4cf54a0cf
Vikash Verma <130314349+Vikashverma2@users.noreply.github.com>
2025-07-02 00:08:42 +0530

    full complete

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Copyright ©2024  <a href="https://github.com/Vikashverma2">Vikash Verma</a>. All rights reserved.</p>
+      <p>Copyright ©2025  <a href="https://github.com/Vikashverma2">Vikash Verma</a>. All rights reserved.</p>
     </footer>
   );
 };
